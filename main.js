27d0810754362eb1d1d4c9e9d89c60af7fa49e82
MF0-ANT1SHY <komeijiddz@gmail.com>
2022-07-15 00:42:05 +0800

    added POW mechanism

--- main.js
+++ main.js
@@ -7,20 +7,30 @@ class Block{
         this.data = data
         this.previousHash = previousHash
         this.hash = this.calculatieHash()
+        this.nonce = 0
     }
     calculatieHash(){
         return SHA256(
             this.index+
             this.timestamp+
             JSON.stringify(this.data)+
-            this.previousHash
+            this.previousHash +
+            this.nonce
         ).toString()
+    }
+    mineBlock(difficulty){
+        while(this.hash.substring(0,difficulty)!==Array(difficulty+1).join('0')){
+            this.hash = this.calculatieHash()
+            this.nonce++
+        }
+        console.log('Block mined:' + this.hash)
     }
 }
 
 class Blockchain {
     constructor(){
         this.chain = [this.createGenesisBlock()]
+        this.difficulty = 4
     }
     createGenesisBlock(){
         return new Block(0, "2022/01/01", "Genesis block", '0')
@@ -30,7 +40,8 @@ class Blockchain {
     }
     addBlock(newBlock){
         newBlock.previousHash = this.getLatestBlock().hash
-        newBlock.hash = newBlock.calculatieHash()
+        // newBlock.hash = newBlock.calculatieHash()
+        newBlock.mineBlock(this.difficulty)
         this.chain.push(newBlock)
     }
     isChainValid(){
@@ -52,7 +63,7 @@ class Blockchain {
 }
 
 let savjeeCoin = new Blockchain()
+console.log('mining block 1...')
 savjeeCoin.addBlock(new Block(1,"2022/01/02", {amount:4}))
+console.log('mining block 2...')
 savjeeCoin.addBlock(new Block(2,"2022/01/02", {amount:4}))
-console.log(savjeeCoin)
-console.log(savjeeCoin.isChainValid())
